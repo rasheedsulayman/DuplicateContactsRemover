4d9fd2f73e3b851d64e2651b6d62749f7110a852
Rasheed Sulayman <sulaymanrasheed@gmail.com>
2019-05-24 16:34:02 +0100

    Initial commit

---
+++ app/src/main/java/com/r4sh33d/duplicatecontactsremover/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.r4sh33d.duplicatecontactsremover
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
