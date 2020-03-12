a2d4a044eb3d191f56618a92308f1ad989adae69
Quadri <quadriyanney@gmail.com>
2020-03-12 11:11:50 +0100

    Removed build files

--- app/build/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.r4sh33d.duplicatecontactsremover.DataBinderMapperImpl());
-  }
-}
